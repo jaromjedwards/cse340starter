afaa5055d47ddd1fc93d25de782685319cc1a2d0
Jarom Edwards <jaromjedwards@gmail.com>
2024-01-25 14:22:01 -0700

    error handling

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const static = require("./routes/static");
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/");
+
 
 /* ***********************
  * View Engine and Templates
@@ -31,11 +33,23 @@ app.set("layout", "./layouts/layout");
 app.use(static);
 
 // index route
-// app.get("/", function (req, res){
-//   res.render("index", {title: "Home"});
-// });
-app.get('/', baseController.buildHome);
+app.get('/', utilities.handleErrors(baseController.buildHome));
 app.use("/inv", inventoryRoute);
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
